076708f320bef420afc2e2ac6e62dd267e5288df
LacusFaustus <your-email@example.com>
2025-11-28 16:05:43 +0300

    Your commit message

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -4,9 +4,14 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.List;
 
 public interface ItemService {
+
     ItemDto createItem(ItemDto itemDto, Long ownerId);
+
     ItemDto updateItem(Long itemId, ItemDto itemDto, Long ownerId);
+
     ItemDto getItemById(Long itemId, Long userId);
+
     List<ItemDto> getAllItemsByOwner(Long ownerId);
+
     List<ItemDto> searchItems(String text, Long userId);
 }
